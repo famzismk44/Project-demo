277c2d819a3469963fb58107ade153434fc52424
famzi <getafam@gail.com>
2025-03-12 16:38:23 -0300

    removed variable from root

--- variable.tf
+++ variable.tf
@@ -4,8 +4,3 @@ variable "instance_id" {
 variable "type" {
   description = "instance type"
 }
-variable "Access_key_ID"
-  description = "access key of aws account"
-
-variable "Secrets_access_key"
-  description = "secrets access key of aws account"
